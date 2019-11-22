0c0c43c98324e01a197287315972394a21a402fd
Seth Todd <seth.todd@outlook.com>
2019-11-22 02:45:04 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
-  puts "   |   "
+  puts " #{board(0)} "   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
